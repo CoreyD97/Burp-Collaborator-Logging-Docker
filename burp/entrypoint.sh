f377935a39b9087bffb7926be963d3b4f64f5075
Corey-Dean Arthur <1339555+CoreyD97@users.noreply.github.com>
2021-05-12 23:20:34 +0100

    Add CollaboratorLogProxy integration

--- burp/entrypoint.sh
+++ burp/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd /opt/burp
+exec java -jar /opt/burp/pkg/CollaboratorLogProxy.jar /opt/burp/conf/LogProxy.properties &
 exec java -jar /opt/burp/pkg/burp.jar --collaborator-server --collaborator-config=/opt/burp/conf/burp.config
